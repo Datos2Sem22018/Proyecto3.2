724e02e5f2e88d094486846266ebba058ed06915
Mariano-CE <marianomm1301@outlook.com>
2018-11-26 13:07:51 -0600

    Progress Bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,20 +38,20 @@ void MainWindow::on_upload_clicked()
     float chunkSize = this->controller->getManager()->GetFileSize(path) / 4000000;
     std::vector<std::string> vecFilenames;
     this->controller->getManager()->split(path, ifs, chunkSize, vecFilenames);
-    if (count == 1) {
+    if (count == 0) {
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 1/A/chunk1.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 2/A/chunk2.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 3/A/chunk3.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk4.bin", "/home/zuckerberg/Escritorio/Disk 4/A/chunk4.bin");
         count++;
     }
-    else if (count == 2) {
+    else if (count == 1) {
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 1/B/chunk1.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 2/B/chunk2.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 3/B/chunk3.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk4.bin", "/home/zuckerberg/Escritorio/Disk 4/B/chunk4.bin");
         count++;
-    } else if (count == 3) {
+    } else if (count == 2) {
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 1/C/chunk1.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 2/C/chunk2.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 3/C/chunk3.bin");
