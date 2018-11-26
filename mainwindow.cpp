f313d84493f20a41a0af46fca6129ee442b2d197
zuckerberg <prieto.luisdaniel@gmail.com>
2018-11-26 12:50:12 -0600

    Salvar la tanda

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -33,24 +33,27 @@ void MainWindow::on_upload_clicked()
     QString filename = QFileDialog::getOpenFileName(this, "Open a File", "", "Video File (*.*)");
     std::string path = filename.toStdString();
     std::ifstream ifs(path);
-    float chunkSize = this->controller->getManager()->GetFileSize(path) / 3000000;
+    float chunkSize = this->controller->getManager()->GetFileSize(path) / 4000000;
     std::vector<std::string> vecFilenames;
     this->controller->getManager()->split(path, ifs, chunkSize, vecFilenames);
     if (count == 0) {
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 1/A/chunk1.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 2/A/chunk2.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 3/A/chunk3.bin");
+        this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk4.bin", "/home/zuckerberg/Escritorio/Disk 4/A/chunk4.bin");
         count++;
     }
     else if (count == 1) {
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 1/B/chunk1.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 2/B/chunk2.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 3/B/chunk3.bin");
+        this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk4.bin", "/home/zuckerberg/Escritorio/Disk 4/B/chunk4.bin");
         count++;
     } else if (count == 2) {
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 1/C/chunk1.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 2/C/chunk2.bin");
         this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 3/C/chunk3.bin");
+        this->controller->getManager()->copy_file("/home/zuckerberg/build-OdysseyMediaPlayer-Desktop_Qt_5_9_1_GCC_64bit-Debug/chunk4.bin", "/home/zuckerberg/Escritorio/Disk 4/C/chunk4.bin");
         count++;
     }
 }
@@ -81,8 +84,8 @@ void MainWindow::on_pushButton_clicked()
         std::string filenameAfter = "Test1.mp4";
         std::ofstream ofs(filenameAfter, std::ios::binary);
         std::vector<std::string> vecFilenames2;
-        std::string paths[3] = {"/home/zuckerberg/Escritorio/Disk 1/A/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 2/A/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 3/A/chunk3.bin"};
-        for (int i = 0; i < 3; ++i) {
+        std::string paths[4] = {"/home/zuckerberg/Escritorio/Disk 1/A/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 2/A/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 3/A/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 4/A/chunk4.bin"};
+        for (int i = 0; i < 4; ++i) {
             vecFilenames2.push_back(paths[i]);
         }
         this->controller->getManager()->join(vecFilenames2, ofs);
@@ -92,8 +95,8 @@ void MainWindow::on_pushButton_clicked()
         std::string filenameAfter = "Test2.mp4";
         std::ofstream ofs(filenameAfter, std::ios::binary);
         std::vector<std::string> vecFilenames2;
-        std::string paths[3] = {"/home/zuckerberg/Escritorio/Disk 1/B/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 2/B/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 3/B/chunk3.bin"};
-        for (int i = 0; i < 3; ++i) {
+        std::string paths[4] = {"/home/zuckerberg/Escritorio/Disk 1/B/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 2/B/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 3/B/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 4/B/chunk4.bin"};
+        for (int i = 0; i < 4; ++i) {
             vecFilenames2.push_back(paths[i]);
         }
         this->controller->getManager()->join(vecFilenames2, ofs);
@@ -103,8 +106,8 @@ void MainWindow::on_pushButton_clicked()
         std::string filenameAfter = "Test3.mp4";
         std::ofstream ofs(filenameAfter, std::ios::binary);
         std::vector<std::string> vecFilenames2;
-        std::string paths[3] = {"/home/zuckerberg/Escritorio/Disk 1/C/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 2/C/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 3/C/chunk3.bin"};
-        for (int i = 0; i < 3; ++i) {
+        std::string paths[4] = {"/home/zuckerberg/Escritorio/Disk 1/C/chunk1.bin", "/home/zuckerberg/Escritorio/Disk 2/C/chunk2.bin", "/home/zuckerberg/Escritorio/Disk 3/C/chunk3.bin", "/home/zuckerberg/Escritorio/Disk 4/C/chunk4.bin"};
+        for (int i = 0; i < 4; ++i) {
             vecFilenames2.push_back(paths[i]);
         }
         this->controller->getManager()->join(vecFilenames2, ofs);
